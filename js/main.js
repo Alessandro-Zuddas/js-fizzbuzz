2351f3f784d3214e5b9a5e29a2d2bfb351e33357
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2022-10-25 10:05:50 +0200

    Corretto spaziatura dentro cicli if

--- js/main.js
+++ js/main.js
@@ -8,25 +8,32 @@ for(let i = 1; i <= 100; i++){
     div.classList.add("col");
 
     if(i % 3 == 0 && i % 5 == 0){
+
         // Se è divisibile sia per 3 che per 5 stampo FizzBuzz
         div.classList.add("col-fizz-buzz");
         div.innerHTML = "FizzBuzz";
         table.append(div);
+
     }else if(i % 3 == 0){
+
         // Se è divisibile per 3 stampo Fizz
         div.classList.add("col-fizz");
         div.innerHTML = "Fizz";
         table.append(div);
+
     }else if(i % 5 == 0){
+
         // Se è divisibile per 5 stampo Buzz
         div.classList.add("col-buzz");
         div.innerHTML = "Buzz";
         table.append(div);
+
     }else{
+
         // Altrimenti stampo il numero contenuto in i
-        console.log(i);
         div.classList.add("col-numbers");
         div.innerHTML = i;
         table.append(div);
+
     }
 }
