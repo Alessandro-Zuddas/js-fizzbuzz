d6888bcc85f8803b75c7366b4a9d12bbd82cbbe7
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2022-10-25 10:21:55 +0200

    Sostituito == uguaglianza rigorosa nelle condiz.

--- js/main.js
+++ js/main.js
@@ -7,21 +7,21 @@ for(let i = 1; i <= 100; i++){
     const div = document.createElement('div');
     div.classList.add("col");
 
-    if(i % 3 == 0 && i % 5 == 0){
+    if(i % 3 === 0 && i % 5 === 0){
 
         // Se è divisibile sia per 3 che per 5 stampo FizzBuzz
         div.classList.add("col-fizz-buzz");
         div.innerHTML = "FizzBuzz";
         table.append(div);
 
-    }else if(i % 3 == 0){
+    }else if(i % 3 === 0){
 
         // Se è divisibile per 3 stampo Fizz
         div.classList.add("col-fizz");
         div.innerHTML = "Fizz";
         table.append(div);
 
-    }else if(i % 5 == 0){
+    }else if(i % 5 === 0){
 
         // Se è divisibile per 5 stampo Buzz
         div.classList.add("col-buzz");
